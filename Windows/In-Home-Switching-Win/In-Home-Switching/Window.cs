c7c36a9544c55953fb45d9cc94b0fb35027357b8
Sun <22406854+SunTheCourier@users.noreply.github.com>
2018-12-18 05:14:53 -0800

    Add Config System (#14)

--- Windows/In-Home-Switching-Win/In-Home-Switching/Window.cs
+++ Windows/In-Home-Switching-Win/In-Home-Switching/Window.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Configsys;
+using System;
 using System.Windows.Forms;
 
 namespace InHomeSwitching.Window
@@ -11,6 +12,7 @@ namespace InHomeSwitching.Window
         {
             streamer = new Streamer();
             InitializeComponent();
+            LoadConfig();
         }
 
         private void Window_Shown(object sender, EventArgs e)
@@ -23,7 +25,7 @@ namespace InHomeSwitching.Window
                     connect_status.Text = $"Connected to {streamer.ip} with bitrate {streamer.quality}M";
                     toggle_button.Text = "Disconnect";
                     quality_bar.Enabled = false;
-                    
+
                 }
                 else
                 {
@@ -44,12 +46,36 @@ namespace InHomeSwitching.Window
             else
             {
                 if (ip_box.Text != string.Empty)
+                {
                     streamer.Start(ip_box.Text, quality_bar.Value);
+                    SaveConfig();
+                }
                 else
                     MessageBox.Show("Please enter the IP-Address of your switch!");
             }
         }
 
-        private void Window_FormClosed(object sender, FormClosedEventArgs e) => Environment.Exit(0);
+        private void Window_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            SaveConfig();
+            Environment.Exit(0);
+        }
+
+        private void LoadConfig()
+        {
+            Config config = new Config();
+            ip_box.Text = config.IP;
+            quality_bar.Value = config.Quality;
+        }
+
+        private void SaveConfig()
+        {
+            Config config = new Config()
+            {
+                IP = ip_box.Text,
+                Quality = quality_bar.Value
+            };
+            config.Save();
+        }
     }
 }
