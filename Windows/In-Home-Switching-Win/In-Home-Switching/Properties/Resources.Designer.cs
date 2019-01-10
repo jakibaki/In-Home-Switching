1c1d39ca344e23a34e32bf7aa5209bde3102dbd3
JiNama <josh.shephard@hotmail.com>
2019-01-10 17:11:46 -0400

    UI Update

--- Windows/In-Home-Switching-Win/In-Home-Switching/Properties/Resources.Designer.cs
+++ Windows/In-Home-Switching-Win/In-Home-Switching/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace DesktopDuplication.Demo.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap InHomeSwitchingCondensed {
-            get {
-                object obj = ResourceManager.GetObject("InHomeSwitchingCondensed", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
