ef651b28bca8a698bb8a05d3cee96e4b1187348e
JiNama <josh.shephard@hotmail.com>
2019-01-10 17:11:46 -0400

    UI Update

--- Windows/In-Home-Switching-Win/In-Home-Switching/Window.Designer.cs
+++ Windows/In-Home-Switching-Win/In-Home-Switching/Window.Designer.cs
@@ -39,18 +39,14 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.quality_bar)).BeginInit();
             this.statusStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // ip_box
             // 
             this.ip_box.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.ip_box.Location = new System.Drawing.Point(19, 246);
+            this.ip_box.Location = new System.Drawing.Point(19, 112);
             this.ip_box.Name = "ip_box";
             this.ip_box.Size = new System.Drawing.Size(304, 20);
             this.ip_box.TabIndex = 1;
@@ -58,7 +54,7 @@
             // quality_bar
             // 
             this.quality_bar.Cursor = System.Windows.Forms.Cursors.Default;
-            this.quality_bar.Location = new System.Drawing.Point(12, 291);
+            this.quality_bar.Location = new System.Drawing.Point(12, 157);
             this.quality_bar.Maximum = 50;
             this.quality_bar.Minimum = 1;
             this.quality_bar.Name = "quality_bar";
@@ -73,7 +69,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Raleway", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(144, 269);
+            this.label3.Location = new System.Drawing.Point(144, 135);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(52, 15);
             this.label3.TabIndex = 8;
@@ -87,9 +83,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.toggle_button.AutoSize = true;
             this.toggle_button.Font = new System.Drawing.Font("Raleway", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.toggle_button.Location = new System.Drawing.Point(68, 146);
+            this.toggle_button.Location = new System.Drawing.Point(65, 18);
             this.toggle_button.Name = "toggle_button";
-            this.toggle_button.Size = new System.Drawing.Size(200, 61);
+            this.toggle_button.Size = new System.Drawing.Size(207, 63);
             this.toggle_button.TabIndex = 10;
             this.toggle_button.TabStop = false;
             this.toggle_button.Text = "Connect";
@@ -100,7 +96,7 @@
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.connect_status});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 339);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 209);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(335, 22);
             this.statusStrip1.TabIndex = 11;
@@ -115,7 +111,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 323);
+            this.label2.Location = new System.Drawing.Point(12, 189);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(22, 13);
             this.label2.TabIndex = 12;
@@ -124,7 +120,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(156, 323);
+            this.label4.Location = new System.Drawing.Point(156, 189);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(28, 13);
             this.label4.TabIndex = 13;
@@ -133,7 +129,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(295, 323);
+            this.label5.Location = new System.Drawing.Point(295, 189);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(28, 13);
             this.label5.TabIndex = 14;
@@ -143,37 +139,16 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Raleway", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(139, 228);
+            this.label1.Location = new System.Drawing.Point(139, 94);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(64, 15);
             this.label1.TabIndex = 15;
             this.label1.Text = "Switch IP:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.pictureBox1.BackgroundImage = global::DesktopDuplication.Demo.Properties.Resources.InHomeSwitchingCondensed;
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(335, 120);
-            this.pictureBox1.TabIndex = 16;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.pictureBox2.Location = new System.Drawing.Point(0, -21);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(335, 84);
-            this.pictureBox2.TabIndex = 17;
-            this.pictureBox2.TabStop = false;
-            // 
             // Window
             // 
-            this.ClientSize = new System.Drawing.Size(335, 361);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(335, 231);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -183,20 +158,18 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.quality_bar);
             this.Controls.Add(this.ip_box);
-            this.Controls.Add(this.pictureBox2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(351, 400);
-            this.MinimumSize = new System.Drawing.Size(351, 400);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(351, 270);
+            this.MinimumSize = new System.Drawing.Size(351, 270);
             this.Name = "Window";
             this.Opacity = 0.99D;
-            this.Text = "In-Home Switching (v0.2)";
+            this.Text = "In-Home Switching";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Window_FormClosed);
             this.Shown += new System.EventHandler(this.Window_Shown);
             ((System.ComponentModel.ISupportInitialize)(this.quality_bar)).EndInit();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -213,8 +186,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
